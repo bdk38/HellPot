54ff8c8a90df804cbbd1f58b4393f7d8dea24728
Wayne King <244781262+bdk38@users.noreply.github.com>
2026-03-08 11:27:25 -0700

    Refactor argument parsing to use flag package
    Refactored argument parsing to use Go's flag package, replacing manual os.Args loop. This resolves the previous TODO regarding CLI improvements.

--- internal/config/arguments.go
+++ internal/config/arguments.go
@@ -1,37 +1,42 @@
 package config
 
 import (
-	"os"
+	"flag"
 )
 
+// forceDebug and forceTrace are set by CLI flags and later override config values.
 var (
 	forceDebug = false
 	forceTrace = false
 )
 
-var argBoolMap = map[string]*bool{
-	"--debug": &forceDebug, "-v": &forceDebug, "--trace": &forceTrace, "-vv": &forceTrace,
-	"--nocolor": &noColorForce, "--banner": &BannerOnly, "--genconfig": &GenConfig,
-}
-
-// TODO: should probably just make a proper CLI with flags or something
+// argParse now uses Go's standard flag package (replaces the old manual os.Args loop).
+// This resolves the previous TODO.
 func argParse() {
-	for i, arg := range os.Args {
-		if t, ok := argBoolMap[arg]; ok {
-			*t = true
-			continue
-		}
-		switch arg {
-		case "-h", "--help":
-			CLI.printUsage()
-		case "-c", "--config":
-			if len(os.Args) < i+2 {
-				println("missing config file after -c/--config")
-				os.Exit(1)
-			}
-			loadCustomConfig(os.Args[i+1])
-		default:
-			continue
-		}
+	var configFile string
+
+	flag.BoolVar(&forceDebug, "debug", false, "enable debug logging")
+	flag.BoolVar(&forceDebug, "v", false, "alias for --debug")
+
+	flag.BoolVar(&forceTrace, "trace", false, "enable trace logging")
+	flag.BoolVar(&forceTrace, "vv", false, "alias for --trace")
+
+	flag.BoolVar(&noColorForce, "nocolor", false, "disable colored output")
+	flag.BoolVar(&BannerOnly, "banner", false, "show banner only and exit")
+	flag.BoolVar(&GenConfig, "genconfig", false, "generate default config and exit")
+
+	flag.StringVar(&configFile, "config", "", "path to custom config file")
+	flag.StringVar(&configFile, "c", "", "path to custom config file (shorthand)")
+
+	// Safe wrapper for help function
+	flag.Usage = func() {
+		CLI.printUsage()
+	}
+
+	flag.Parse()
+
+	// If a custom config was specified, load it immediately
+	if configFile != "" {
+		loadCustomConfig(configFile)
 	}
 }
